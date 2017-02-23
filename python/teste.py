fb2a25a0352b022e023d76e685dfd996964ea4af
Samuka <Samuka>
2017-02-23 20:13:51 -0300

    Teste

---
+++ python/teste.py
@@ -0,0 +1,28 @@
+num = 13
+nome = "Samuel"
+hello = "Hello"
+
+print(hello)
+print(hello + "Word")#concatenação
+print(hello*3)#repetição
+print(hello[0])#Primeiro Indice
+print(hello[-1])#Último indice
+print(len(hello))
+print('e' in hello)
+var = "Centro Universitário 7 de Setembro - UNI7"
+print(var[1:7])
+print(var[1:7:2])
+print(var[::-1])
+print(var.split())
+print(var.count("7"))
+var2 = var.split()
+print("/".join(var2))
+tupla = ("Bom", "Dia")  #Imutável
+lista = ["Boa"]
+
+
+
+
+
+
+input("Digite Enter")
