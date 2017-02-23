74517ddf555b9c61277b1141fbaffd9f82af6c95
israel portela ferreira <1321403>
2017-02-23 20:38:55 -0300

    incluindo teste

---
+++ py/testeA.py
@@ -0,0 +1,16 @@
+#print("Israel")
+
+num = 13
+nome = "Israel"
+hello = "Hello"
+print(hello)
+print(hello + " World")
+print(" " + hello*3)
+print(hello[0])
+print(hello[-1])
+print(len(hello))
+print('e' in hello)
+var = "Faculdade 7 de Setembro"
+print(var[1:7])
+print(var[1:7:2])
+print(var[::-1])
