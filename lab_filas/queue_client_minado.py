bbd7ed4f3c6f5c921a6b1983f556a305f6d67a54
ChelderStark <helderdroid@gmail.com>
2017-05-16 18:07:31 -0300

    Atualização do Campo Minado com Queue

---
+++ lab_filas/queue_client_minado.py
@@ -0,0 +1,141 @@
+import zmq
+import sys
+import random, os
+
+port = "5559"
+context = zmq.Context()
+print ("Conectando com o servidor...")
+
+socket = context.socket(zmq.REQ)
+socket.connect ("tcp://localhost:%s" % port)
+client_id = random.randrange(1,10005)
+
+def start_game():
+    game = True
+    cont = 0
+
+    linha = ''
+    colunas = ''
+    minas = ''
+
+    campoComMinas = []
+    campoSemMinas = []
+    print("\tBem vindo ao Campo minado!!")
+    if os.path.exists('salvo.pkl') == True:
+        print("\a\a\a\a Carregando Jogo Salvo!")
+        print("\t\tJogo Carregado, Continue!")
+        dados = ["C", 'salvo.pkl']
+        socket.send_json(dados)
+
+        data = socket.recv_json()
+        cont, linha, colunas, campoComMinas = dados
+        cont = int(cont)
+
+
+    else:
+        print("\a\a\a\a Iniciando Novo Jogo...")
+        linhas = input('Digite a quantidade de linhas: ')
+        colunas = input('Digite a quantidade de colunas: ')
+        minas = input('Digite a qtd de Minas: ')
+
+        #cria um tabuleiro em branco
+        dados = ["I", linhas, colunas]
+        socket.send_json(dados)
+        campoSemMinas = socket.recv_json()
+
+        #põe minas escondidas no tabuleiro
+        dados = ["M", linhas, colunas, minas, campoSemMinas]
+        socket.send_json(dados)
+        campoComMinas = socket.recv_json()
+
+
+    while game:
+        mostraCampoSMinas(campoSemMinas, linha, colunas)
+
+        print("\nJogadas: %d" % cont)
+        cont += 1
+
+        print("Se desejar encerrar o jogo digite uma letra, no lugar da linha!")
+        hitL = input('\tEscolha a linha: ')
+        hitC = input("\tEscolha a Coluna: ")
+
+        game = escolha(hitL, hitC, campoSemMinas, campoComMinas)                #executa a função escolha e retorna um valor de opção
+        if game == 1:
+            os.system("cls")
+            salvar(str(cont), linha, colunas, campoComMinas)
+            continue
+        elif game == 2:                                                        #caso seja escolhido uma letra ao invés de numero ele salva
+            os.system("cls")
+            os.system("color 0F")
+            salvar(str(cont), linha, colunas, campoComMinas)
+            print("\n\tJogo salvo, Jogo encerado!!!")
+            break
+        else:
+            os.system("cls")
+            os.system("color 0C")
+            print("\t\t Jogo finalizado!!!")
+            print("\tVocê acertou uma mina em %d jogadas." % cont)
+            mostraCampoComMinas(campoComMinas, linha, colunas)
+            finaliza_jogo(cont)                                             #executa a função de finalização do game
+            print("\n\a\a\a....Arquivo salvo deletado com sucesso...\a\a\a")
+            break
+
+
+
+def mostraCampoSMinas(campo, linha, coluna):
+    cont = 1
+    col = "\a| "
+
+    for i in range(1, int(coluna) + 1):
+        col += ("\t%s" % i)
+    print(col)
+
+    for i in campo:
+        linha = i
+        l1 = ("%d| " % cont)
+        for j in linha:
+            l1 += ("\t%s" % j)
+        print(l1)
+        cont += 1
+
+def mostraCampoComMinas(campo, linha, coluna):
+    cont = 1
+    col = "\a| "
+    for i in range(1, int(coluna) + 1):
+        col += ("\t%s" % i)
+    print(col)
+    for i in campo:
+        linha = i
+        l1 = ("%d| " % cont)
+        for j in linha:
+            l1 += ("\t%s" % j)
+        print(l1)
+        cont += 1
+
+def escolha(linha, coluna, matrizB, matrizM):
+
+    if linha.isnumeric() and coluna.isnumeric():       #verifica se os dois valores são numéricos
+
+        if matrizM[int(linha)-1][int(coluna)-1] == "B":#verifica se nas coordenadas existe mina
+            return 0
+        else:
+            matrizM[int(linha)-1][int(coluna)-1] = "*"
+            matrizB[int(linha)-1][int(coluna)-1] = "*"
+            return 1
+    else:
+        return 2
+
+#Envia o arquivo para ser salvo no server
+
+def salvar(jogadas, linha, coluna, campo):
+    dados = ["S", jogadas, linha, coluna, campo]
+    socket.send_json(dados)
+
+#executa a finalização do jogo no server
+def finaliza_jogo(jogadas):
+    dados = ["F", jogadas]
+    socket.send_json(dados)
+
+
+if __name__ == '__main__':
+    start_game()
--- lab_filas/queue_server.py
+++ lab_filas/queue_server.py
@@ -1,7 +1,8 @@
 import zmq
 import time
 import sys
-import random
+import random, pickle
+import os, os.path
 
 try:
     port = "5560"
@@ -11,16 +12,104 @@ try:
     server_id = random.randrange(1,10005)
 
     while True:
-        #  Espera pela próxima requisição do cliente
-        message = socket.recv()
-        print("Recebi requisição de : ", message)
-        time.sleep (1)
-        text = "Id do servidor %s" % server_id
-        data = text.encode("UTF-8")
-        socket.send(data)
+
+        message = socket.recv_json()
+        cod, *dados = message
+
+        # inicializa um novo jogo e envia para o cliente
+        if cod == "I":
+            linha, coluna = dados
+            campo = []
+            for i in range(0, int(linha)):
+                linha = []
+                for j in range(0, int(coluna)):
+                    linha.append(0)
+
+                campo.append(linha)
+            data = campo
+            socket.send_json(data)
+
+        # distribui as minas no tabuleiro
+        elif cod == "M":
+            linha, coluna, minas, matriz = dados
+            for i in range(1, int(minas) + 1):
+                p = random.randint(0, int(linha) - 1)  # escolhe uma posição na linha aleatória
+                q = random.randint(0, int(coluna) - 1)  # escolhe uma posição na coluna aleatória
+                if matriz[p][q] != 'B':
+                    matriz[p][q] = 'B'
+            data = matriz
+            socket.send_json(data)
+
+        # salva os dados no arquivo de forma codificada
+        elif cod == "S":
+            jogadas, linhas, colunas, campo = dados
+            arq = open("salvo.zmq", 'w')
+            arq.write(jogadas)
+            arq.write('\n')
+            arq.write("%d" % linhas)
+            arq.write(',')
+            arq.write("%d" % colunas)
+            arq.write(',')
+            arq.write('\n')
+            for i in campo:
+                linha = ""
+                for j in i:
+                    linha += ("%s" % j)
+                arq.write(linha)
+                arq.write('\n')
+            arq.close()
+
+        # finaliza o jogo deletando o arquivo existente salvo
+        elif cod == "F":
+            if os.path.exists("salvo.zmq"):
+                print("\t\t Jogo finalizado!!!")
+                os.unlink('salvo.zmq')
+                print("Jogo salvo foi deletado pelo Server!!")
+
+        # carrega um jogo salvo e envia para o cliente
+        elif cod == "C":
+            campo = []
+            camp2 = []
+            arq = open("salvo.zmq", 'r')
+            linha1 = arq.readline()  # lê a primeira linha que está as jogadas
+            linha2 = arq.readline()  # lê a segunda linha que esta as linhas e colunas
+            linha2 = linha2.split(",")  # cria uma lista dividida da linha 2
+            linha = int(linha2[0])  # pega a primeira posição e joga em linha
+            coluna = int(linha2[1])  # pega a segunda posição e joga em coluna
+
+            for lin in arq:  # pega o arquivo salvo e transforma em matriz
+                linhas = []
+                for i in lin:
+                    if i != '\n':
+                        linhas.append(i)
+                campo.append(linhas)
+                camp2.append(linhas)
+
+                # faz a verificação do campo que foi carregado e cria um novo campo
+                # sobreposto para mascarar as minas
+
+            def mapeamento(x):
+                if x == "B":  # troca as minas por "0" quando verdadeiro
+                    x = '0'
+                    return x
+                else:
+                    return str(x)
+
+            # carrega o campo sobreposto
+            campSobrePosto = []
+            for lin in camp2:
+                linha = []
+                for i in lin:
+                    linha.append(mapeamento(i))
+                campSobrePosto.append(linha)
+
+            arq.close()
+            dados = int(linha1), campo, campSobrePosto  # retorna as jogadas e os campos
+            socket.send_json(dados)
+
 
 except:
     for val in sys.exc_info():
         print(val)
 
-input("Saida Enter")
+
