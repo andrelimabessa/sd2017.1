966d86463cfa88a302152fb70e20f8c52b25e542
matheuscsmg <matheusds.sousa@gmail.com>
2017-05-16 23:31:52 -0300

    Campo Minado RPC

--- campominado.py
+++ campominado.py
@@ -3,139 +3,9 @@ import random
 import re
 import time
 from string import ascii_lowercase
+from jsonrpclib import Server
 
-
-def criar_tabuleiro(tamanho_tabuleiro, inicio, qtd_minas):
-    tabuleiro_vazio = [['0' for i in range(tamanho_tabuleiro)] for i in range(tamanho_tabuleiro)]
-
-    minas = pega_minas(tabuleiro_vazio, inicio, qtd_minas)
-
-    for i, j in minas:
-        tabuleiro_vazio[i][j] = 'X'
-
-    tabuleiro = pega_numeros(tabuleiro_vazio)
-
-    return (tabuleiro, minas)
-
-
-def mostrar_tabuleiro(tabuleiro):
-    tamanho_tabuleiro = len(tabuleiro)
-
-    horizontal = '   ' + (4 * tamanho_tabuleiro * '-') + '-'
-
-    # Mostrar as letras das colunas
-    label_colunas = '     '
-
-    for i in ascii_lowercase[:tamanho_tabuleiro]:
-        label_colunas = label_colunas + i + '   '
-
-    print(label_colunas + '\n' + horizontal)
-
-    # Mostrar os números das linhas
-    for idx, i in enumerate(tabuleiro):
-        linha = '{0:2} |'.format(idx + 1)
-
-        for j in i:
-            linha = linha + ' ' + j + ' |'
-
-        print(linha + '\n' + horizontal)
-
-    print('')
-
-
-def pega_celula_aleatoria(tabuleiro):
-    tamanho_tabuleiro = len(tabuleiro)
-
-    a = random.randint(0, tamanho_tabuleiro - 1)
-    b = random.randint(0, tamanho_tabuleiro - 1)
-
-    return (a, b)
-
-
-def pega_vizinhos(tabuleiro, num_linha, num_coluna):
-    tamanho_tabuleiro = len(tabuleiro)
-    vizinhos = []
-
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if i == 0 and j == 0:
-                continue
-            elif -1 < (num_linha + i) < tamanho_tabuleiro and -1 < (num_coluna + j) < tamanho_tabuleiro:
-                vizinhos.append((num_linha + i, num_coluna + j))
-
-    return vizinhos
-
-
-def pega_minas(tabuleiro, inicio, qtd_minas):
-    minas = []
-    vizinhos = pega_vizinhos(tabuleiro, *inicio)
-
-    for i in range(qtd_minas):
-        celula = pega_celula_aleatoria(tabuleiro)
-        while celula == inicio or celula in minas or celula in vizinhos:
-            celula = pega_celula_aleatoria(tabuleiro)
-        minas.append(celula)
-
-    return minas
-
-
-def pega_numeros(tabuleiro):
-    for num_linha, linha in enumerate(tabuleiro):
-        for num_coluna, celula in enumerate(linha):
-            if celula != 'X':
-                # Pega os valores dos vizinhos
-                valor = [tabuleiro[l][c] for l, c in pega_vizinhos(tabuleiro, num_linha, num_coluna)]
-
-                # Contar quantas são minas
-                tabuleiro[num_linha][num_coluna] = str(valor.count('X'))
-
-    return tabuleiro
-
-
-def mostra_celulas(tabuleiro, tabuleiro_atual, num_linha, num_coluna):
-    # Sai da função se a célula já apareceu
-    if tabuleiro_atual[num_linha][num_coluna] != ' ':
-        return
-
-    # Mostra célula atual
-    tabuleiro_atual[num_linha][num_coluna] = tabuleiro[num_linha][num_coluna]
-
-    # Pega os vizinhos se a célula estiver vazia
-    if tabuleiro[num_linha][num_coluna] == '0':
-        for l, c in pega_vizinhos(tabuleiro, num_linha, num_coluna):
-            # Repete a função pra cada vizinho que não tem uma bandeira
-            if tabuleiro_atual[l][c] != 'F':
-                mostra_celulas(tabuleiro, tabuleiro_atual, l, c)
-
-
-def jogar_novamente():
-    escolha = input('Jogar novamente? (s/n): ')
-
-    return escolha.lower() == 's'
-
-    os.system('cls')
-
-def analisar_entrada(string_entrada, tamanho_tabuleiro, mensagem_ajuda):
-    celula = ()
-    bandeira = False
-    mensagem = "Célula inválida. " + mensagem_ajuda
-
-    padrao = r'([a-{}])([0-9]+)(f?)'.format(ascii_lowercase[tamanho_tabuleiro - 1])
-    entrada = re.match(padrao, string_entrada)
-
-    if string_entrada == 'ajuda':
-        mensagem = mensagem_ajuda
-
-    elif entrada:
-        num_linha = int(entrada.group(2)) - 1
-        num_coluna = ascii_lowercase.index(entrada.group(1))
-        bandeira = bool(entrada.group(3))
-
-        if -1 < num_linha < tamanho_tabuleiro:
-            celula = (num_linha, num_coluna)
-            mensagem = ''
-
-    return {'celula': celula, 'bandeira': bandeira, 'mensagem': mensagem}
+proxy = Server('http://localhost:7002')
 
 
 def jogar():
@@ -161,13 +31,13 @@ def jogar():
 
     mensagem_ajuda = ("Digite a coluna seguida da linha (exemplo: a3). \n" "Para colocar ou remover uma bandeira, digite 'F' após a célula (exemplo: a3f).\n")
 
-    mostrar_tabuleiro(tabuleiro_atual)
+    proxy.mostrar_tabuleiro(tabuleiro_atual)
     print(mensagem_ajuda + "Digite 'ajuda' para mostrar essa mensagem novamente.\n")
 
     while True:
         minas_restantes = qtd_minas - len(bandeiras)
         jogada = input('Escolha a próxima célula ({} mina(s) restante(s)): '.format(minas_restantes))
-        resultado = analisar_entrada(jogada, tamanho_tabuleiro, mensagem_ajuda + '\n')
+        resultado = proxy.analisar_entrada(jogada, tamanho_tabuleiro, mensagem_ajuda + '\n')
 
         mensagem = resultado['mensagem']
         celula = resultado['celula']
@@ -181,7 +51,7 @@ def jogar():
             bandeira = resultado['bandeira']
 
             if not tabuleiro:
-                tabuleiro, minas = criar_tabuleiro(tamanho_tabuleiro, celula, qtd_minas)
+                tabuleiro, minas = proxy.criar_tabuleiro(tamanho_tabuleiro, celula, qtd_minas)
             if not tempo_inicio:
                 tempo_inicio = time.time()
 
@@ -204,13 +74,13 @@ def jogar():
             elif tabuleiro[num_linha][num_coluna] == 'X':
 
                 print('Você perdeu! :(\n')
-                mostrar_tabuleiro(tabuleiro)
-                if jogar_novamente():
+                proxy.mostrar_tabuleiro(tabuleiro)
+                if proxy.jogar_novamente():
                     jogar()
                 return
 
             elif celula_atual == ' ':
-                mostra_celulas(tabuleiro, tabuleiro_atual, num_linha, num_coluna)
+                proxy.mostra_celulas(tabuleiro, tabuleiro_atual, num_linha, num_coluna)
 
             else:
                 mensagem = "Esta célula já foi liberada."
@@ -220,13 +90,13 @@ def jogar():
                 print(
                     'Você venceu! :D\n'
                     'Esta partida demorou {} minutos e {} segundos para ser finalizada!\n'.format(minutos, segundos))
-                mostrar_tabuleiro(tabuleiro)
+                proxy.mostrar_tabuleiro(tabuleiro)
                 if jogar_novamente():
                     jogar()
                 return
 
         os.system('cls')
-        mostrar_tabuleiro(tabuleiro_atual)
+        proxy.mostrar_tabuleiro(tabuleiro_atual)
         print(mensagem)
 
 jogar()
